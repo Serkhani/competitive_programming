3e7c3ed94c4e74b0a028efeb26fd72817a7b95bb
Serkhani <deviper03@gmail.com>
2023-05-25 20:48:02 +0000

    added weird not weird

---
+++ weird_notWierd.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if (n%2==1):
+        print("Weird")
+    else:
+        # if n>1 and n<6:
+        #     print("Not Weird")
+        # el
+        if n>5 and n<21:
+            print("Weird")
+        else:
+            print("Not Weird")
