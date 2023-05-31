90adbd305e3263e251ed943df5efe8bf646bba33
Serkhani <deviper03@gmail.com>
2023-05-31 20:34:38 +0000

    add set_union

---
+++ set_union.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+englishCount = input()
+englishSet = set(input().split())
+frenchCount = input()
+frenchSet = set(input().split())
+print(len(englishSet.union(frenchSet)))
