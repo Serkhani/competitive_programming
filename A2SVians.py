9d58000533f0279ec4b19df1c3fe8bf8b97f5865
Serkhani <deviper03@gmail.com>
2023-06-02 20:56:48 +0000

    optimizing

--- A2SVians.py
+++ A2SVians.py
@@ -1,12 +1,9 @@
+from collections import Counter
 n = int(input())
-members = set(input().split())
-badMembers = set(input().split())
+goodMembers = set(input().split()) - set(input().split())
 excellent_count = 0
-for member in members:
-    if member not in badMembers:
-        char_count = {}
-        for char in member:
-            char_count[char] = char_count.get(char, 0) + 1
-        if len(set(char_count.values())) == 1:
-            excellent_count += 1
+for member in goodMembers:
+    char_count = Counter(member)
+    if len(set(char_count.values())) == 1:
+        excellent_count += 1
 print(excellent_count)
