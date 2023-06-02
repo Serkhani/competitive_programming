033344bcbe8ebf8c6f2d8fb24b4a0ff0d0052dcc
Serkhani <deviper03@gmail.com>
2023-06-02 15:40:17 +0000

    added trafficlight

---
+++ trafficlight.py
@@ -0,0 +1,17 @@
+# 6
+# 5 r
+# rggry
+# 1 g
+# g
+# 3 r
+# rrg
+# 5 y
+# yrrgy
+# 7 r
+# rgrgyrg
+# 9 y
+# rrrgyyygy
+t = int(input())
+for _ in range(t):
+    n, c = input().split()
+    n = int(n)
