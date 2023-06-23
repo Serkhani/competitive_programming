ea24f0d963c2378b598cab4bcb7df082e0fa2e8c
Serkhani <deviper03@gmail.com>
2023-06-23 21:26:00 +0000

    added some update

--- a2sv-contest3/Ilya_and_Bank_Account.py
+++ a2sv-contest3/Ilya_and_Bank_Account.py
@@ -8,3 +8,33 @@ else:
     balance //= 10
     balance += 0 if hundreds<ones else (ones-hundreds)  
     print(-balance)
+# # balance_str = input()
+# balance = int(input())
+# if balance >= 0:
+#     print(balance)
+# else:
+#     # if len(balance_str) == 1: print("0")
+#     # maxbal = -math.inf
+#     # i = 1
+#     # while i<len(balance_str):
+#     #     if balance_str[i] != '0':
+#     #         data = balance_str[:i] + balance_str[i+1:]
+#     #         print(data)
+#     #         maxbal = max(abs(int(data)),maxbal)
+#     #     i+=1
+#     # print(-maxbal)
+#     # if balance>-10:
+#     #     print("0")
+#     # else:
+#         balance *= -1
+#         ones = balance%10
+#         hundreds = (balance%100)//10
+#         balance //= 10
+#         balance += 0 if hundreds<ones else (ones-hundreds)
+            
+
+#         print(-balance)
+        
+
+# # thught it was supposed to be any digit
+# # tried to use string
