c039a43d147a27debb79c9744cdbec2e5fe30b38
Serkhani <deviper03@gmail.com>
2023-06-30 05:01:34 +0000

    added sort people

---
+++ leetcode/sort_people.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        for i in range(len(heights)):
+            swap = False
+            for j in range(len(heights)-i-1):
+                if heights[j]<heights[j+1]:
+                    swap = True
+                    heights[j], heights[j+1] = heights[j+1], heights[j]
+                    names[j], names[j+1] = names[j+1], names[j]
+            if not swap:
+                break
+        return names
+                    
