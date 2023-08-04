27d5b9a8bc61a63afe794b773d4797eead87b851
Serkhani <deviper03@gmail.com>
2023-08-04 17:36:56 +0100

    added find_the_prefix_common_array_of_two_arrays.

---
+++ leetcode/find_the_prefix_common_array_of_two_arrays.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        commons = set()
+        count = 0
+        ptr = 0
+        arr = []
+        while ptr<len(A):
+            if A[ptr]==B[ptr]:
+                count+=1
+                commons.add(A[ptr])
+                commons.add(B[ptr])
+            else:
+                if A[ptr] in commons:
+                    count+=1
+                else: commons.add(A[ptr])
+                if B[ptr] in commons:
+                    count+=1
+                else:commons.add(B[ptr])
+            
+            arr.append(count)
+            ptr+=1
+        return arr
